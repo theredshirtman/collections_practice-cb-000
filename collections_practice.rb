e7241ab6008eb53befc0a5bdb22111e083107cba
Oghenevwede Agboro-Jimoh <oaagboro@syr.edu>
2017-03-27 20:51:09 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  one = array[1]
+    two = array[2]
+
+    array[1] = two
+    array[2] = one
+
+  return  array
+
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |element|
+
+     element[2] = "$"
+
+    end
+ return array
+end
+
+def find_a(array)
+  array.select {|str| str.start_with?("a")}
+end
+
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect {|val, index| index != 1 ? val + "s" : val}
+end
